22a45d5e052b8339a76c539e182f20fd3a164f1e
Ketan N <ketan.nayak@gmail.com>
2016-12-24 15:31:28 -0800

    text to speech

--- joke-print.py
+++ joke-print.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+import os
 
 ret = urllib2.urlopen("http://api.icndb.com/jokes/random").read()
 joke = json.loads(ret)
@@ -7,3 +8,5 @@ joke = json.loads(ret)
 joke_string = joke["value"]["joke"]
 
 print(joke_string)
+os.system('say Okay. Here is a joke.')
+os.system('say '+ joke_string)
