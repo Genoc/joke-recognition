3c186b004b387a3e72956cb2596b01aeb35aa03b
Ketan N <ketan.nayak@gmail.com>
2016-12-25 15:04:42 -0800

    pole latest

--- FrozenLake/cartpoleKN.py
+++ FrozenLake/cartpoleKN.py
@@ -1,6 +1,23 @@
 import gym
+import numpy as np
+import tensorflow
 env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+
+
+def run_episode(env, parameters):  
+    observation = env.reset()
+    totalreward = 0
+    for _ in xrange(200):
+        env.render()
+        action = 0 if np.matmul(parameters,observation) < 0 else 1
+        observation, reward, done, info = env.step(action)
+        totalreward += reward
+        if done:       
+		    return totalreward
+
+for i_episode in range(20):
+    parameters = np.random.rand(4) * 2 - 1  
+    reward = run_episode(env, parameters)
+    print(parameters)
+    print(reward)
+
