a4e492f7ce6bd84cc9f6d91706d7750cb7cea437
Nitin Viswanathan <nitin.viswanathan@gmail.com>
2016-12-24 17:45:38 -0800

    cleaned up repo

--- brooklyn-curl.sh
+++
@@ -1,4 +0,0 @@
-curl -s -k -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $GUAC_AUTH" \
-    https://speech.googleapis.com/v1beta1/speech:syncrecognize \
-    -d @sync-request.json
